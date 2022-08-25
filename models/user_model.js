7949e4643f4daa78cc36b12220fba13cb9a5871f
bloodwarriorr <kenar529@gmail.com>
2022-08-25 13:13:55 +0300

    changed user model empty arr ranks

--- models/user_model.js
+++ models/user_model.js
@@ -14,7 +14,7 @@ class User {
         this.nickname=nickname;
         this.email=email;
         this.password = password;
-        this.level_rank = [{"level_code":1,"rank":0}]
+        this.level_rank = []
         this.avatarCode = avatarCode;
         this.current_level = 1;
         this.gender = gender;
