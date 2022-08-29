34a491a55dacbdaadf3f29a6fe620018956da487
bloodwarriorr <kenar529@gmail.com>
2022-08-29 16:31:22 +0300

    add error message

--- controllers/reset_password_controller.js
+++ controllers/reset_password_controller.js
@@ -9,19 +9,19 @@ const bcrypt = require('bcryptjs');
 
 router.post("/reset", async (req, res) => {
     try {
-    let { email } = req.body; //get the id param.
+        let { email } = req.body; //get the id param.
 
-    let user = await new DB().FindByEmail("users", email);
-        if(!user){
+        let user = await new DB().FindByEmail("users", email);
+        if (!user) {
             return res.status(400).send("User not found!");
         }
-        let token = await new DB().FindByUserId("token",user._id.toString())
-        
+        let token = await new DB().FindByUserId("token", user._id.toString())
+
         if (!token) {
-            token=new Token(user._id,crypto.randomBytes(32).toString("hex"))
+            token = new Token(user._id, crypto.randomBytes(32).toString("hex"))
             await new DB().Insert("token", token);
         }
-       
+
         const link = `${process.env.BASE_URL}/password-reset/?id=${user._id}&token=${token.token}`;
         await sendEmail(user.email, "Click on the link to reset your password", link);
         res.send("password reset link sent to your email account");
@@ -33,21 +33,18 @@ router.post("/reset", async (req, res) => {
 
 router.post("/:userId/:token", async (req, res) => {
     try {
-        const user = await new DB().FindByID("users",req.params.userId);
-       
+        const user = await new DB().FindByID("users", req.params.userId);
         if (!user) return res.status(400).send("invalid link or expired");
-        
-        const token = await new DB().FindByUserId("token",user._id.toString())
-       
+        const token = await new DB().FindByUserId("token", user._id.toString())
         if (!token) return res.status(400).send("Invalid link or expired");
         encryptedPassword = await bcrypt.hash(req.body.password, 10);
         user.password = encryptedPassword
-        await new DB().UpdateDocById("users",user._id,user)
-        await new DB().DeleteDocById("token",token._id.toString())
+        await new DB().UpdateDocById("users", user._id, user)
+        await new DB().DeleteDocById("token", token._id.toString())
         res.send("password reset sucessfully.");
     } catch (error) {
-        res.send("An error occured");
-        console.log(error);
+        return res.status(400).send("Something went wrong during reset password");
+
     }
 });
 
