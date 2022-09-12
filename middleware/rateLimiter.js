db4364eee03e57ef7c34f40290b588b6c51fe124
bloodwarriorr <kenar529@gmail.com>
2022-09-12 14:38:42 +0300

    sss

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const loginRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 min in milliseconds
+  windowMs: 1 * 60 * 1000, // 15 min in milliseconds
   max: 5,
   message: 'Login error, you have reached maximum retries. Please try again after 30 minutes', 
   statusCode: 429,
